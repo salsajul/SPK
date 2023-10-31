86944e1b7dc4c2cac28ca19f44a09a4fdc919883
Salsa Juliani <103833785+salsajul@users.noreply.github.com>
2023-10-31 17:22:22 +0700

    Add files via upload

---
+++ UTS SPK SALSA JULIANI NURYADIN/Salsa Juliani Nuryadin_201011400895.sql
@@ -0,0 +1,11 @@
+INSERT INTO laptop (nama,prosesor,ram,"storage",baterai,harga) VALUES
+	 ('Lenovo LOQ 15','Intel Core i5-13420H - 2.5 Ghz',8,512,60,18499000),
+	 ('Lenovo Yoga Book 9i','Intel Core i7-1355U - 2.4 Ghz',16,1,80,34999000),
+	 ('Lenovo IdeaPad Slim 5','AMD Ryzen 5 7530U-2.1 Ghz',16,512,57,9099000),
+	 ('Lenovo Yoga 6','AMD Ryzen 7 7840H-2.1 Ghz',16,512,59,13999000),
+	 ('Lenovo Legion Slim 51','AMD Ryzen 5 7640HS - 2.1 Ghz',8,512,80,21999000),
+	 ('Lenovo LOQ','Intel Core i5-13420H - 2.5 Ghz',8,512,60,18500000),
+	 ('Lenovo IdeaPad Flex 5','Intel Core i5-1335U-1.7 Ghz',16,512,53,13499000),
+	 ('Lenovo Legion Pro 5','Intel Core i7-13700HX - 2.6Ghz',8,1,80,22999000),
+	 ('Lenovo Yoga Pro 71','Intel Core i7-13700H - 2.6Ghz',16,1,73,22999000),
+	 ('Lenovo Yoga Pro 7','Intel Core i5-13500H - 2.7Ghz',16,512,73,15499000);
